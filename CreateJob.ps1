a3774b3856c9888cadf3131e2a2c9b7f5036f644
Joppe Catsburg <2551256+catsburg@users.noreply.github.com>
2021-11-29 08:55:28 +0100

    Create CreateJob.ps1

---
+++ CreateJob.ps1
@@ -0,0 +1,55 @@
+param (
+    [Parameter(Mandatory = $true)]$IsFinal
+)
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$GrantType = "client_credentials"
+$ClientId = "dc7e16c9-85b8-47c2-861c-0c54d5a3eec2"
+$ClientSecret = "T197Q~2eoteJMIqdyfa5z2yFvYdqli87wyXkd"
+$tenantId = "c88cf34a-896c-41b7-9880-66ac0bf45312"
+$Scope = "api://e29099a5-3119-455e-83d1-78c796881e0a/.default"
+
+$body = @{
+ "grant_type"="$GrantType"
+ "client_id"="$ClientId"
+ "client_secret"="$ClientSecret"
+ "scope"="$Scope"
+ }
+
+ $tokenRequestResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Method 'Post' -Body $body -ContentType "application/x-www-form-urlencoded"
+ $token = $tokenRequestResponse.access_token
+
+ $headers = @{
+    Authorization="Bearer $token"
+}
+
+$JobRequestObject = @{}
+$JobRequestObject.pipelineType = "GithubWorkflow"
+$JobRequestObject.scope = "catsburg:ReleaseDocumentation"
+$JobRequestObject.runId = $Env:GITHUB_RUN_ID
+$JobRequestObject.isFinal = [System.Convert]::ToBoolean($IsFinal)
+
+$JobRequestJson = $(ConvertTo-Json -InputObject $JobRequestObject -Compress)
+$JobRequestResponse = Invoke-WebRequest -Method 'Post' -Uri "https://releasedoc-api.azurewebsites.net/RunDocumentationJobs" -ContentType application/json -Headers $headers -Body $JobRequestJson
+
+$JobStatusRequestResponse = Invoke-WebRequest -Uri $JobRequestResponse.Headers.Location -Method GET -Headers $headers -ContentType application/json
+$JobStatusRequestResponseJson  = $JobStatusRequestResponse.Content | Out-String | ConvertFrom-Json 
+$Status =  $JobStatusRequestResponseJson.status
+$CreatedRunId = $JobStatusRequestResponseJson.createdRunId
+
+Write-Output $JobStatusRequestResponseJson
+
+while($Status -ne "Completed")
+     {
+       $JobStatusRequestResponse = Invoke-WebRequest -Uri $JobRequestResponse.Headers.Location -Method GET -Headers $headers -ContentType application/json
+       $JobStatusRequestResponseJson  = $JobStatusRequestResponse.Content | Out-String | ConvertFrom-Json 
+       $Status =  $JobStatusRequestResponseJson.status
+       $CreatedRunId = $JobStatusRequestResponseJson.createdRunId
+       Write-Output $Status
+
+       Start-Sleep -s 1
+     }
+
+
+Write-Output "https://releasedoc.azurewebsites.net/Runs/$CreatedRunId"
